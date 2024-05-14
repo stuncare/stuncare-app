6c79ed42b7b527bf47fcb7e38e978f12ec0aeb56
Stuncare <stuncareid@gmail.com>
2024-05-15 04:54:16 +0700

    first commit

---
+++ app/src/main/java/com/widyawacana/stuncare/ui/presentation/beranda/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.widyawacana.stuncare.ui.presentation.beranda
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
---
+++ app/src/main/java/com/widyawacana/stuncare/ui/presentation/login/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.widyawacana.stuncare.ui.presentation.login
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoginScreen() {
+
+}
